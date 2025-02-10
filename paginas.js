6e25377dfbea9ae76edf058b7e9e4efbda1430f1
Carolz14 <161259026+Carolz14@users.noreply.github.com>
2025-02-10 15:50:11 -0300

    Add files via upload

---
+++ paginas.js
@@ -0,0 +1,43 @@
+/*Trocar de aba na pagina */
+
+
+
+function exibePagina(idDaPagina) {
+    
+    document.querySelectorAll('.aba, #menu a')
+      .forEach(function(el) {
+        el.classList.remove('ativa');
+      });
+  
+   
+    const paginaParaExibirEl = document.querySelector(idDaPagina);
+  
+
+   paginaParaExibirEl.classList.add('ativa');
+  
+    document.querySelector('#menu a[href="' + idDaPagina + '"]')
+      .classList.add('ativa');
+  }
+  
+
+  const itensDoMenu = document.querySelectorAll('#menu a');
+  itensDoMenu.forEach(function(el) {
+    el.addEventListener('click', function(evento) {
+
+      const hrefDoLink = evento.target.href;
+      const idApontadoPeloLink = hrefDoLink.substr(hrefDoLink.lastIndexOf('#'));
+  
+      exibePagina(idApontadoPeloLink);
+    });
+  });
+  
+
+  if (location.hash) {
+    const hrefDosItensDeMenu = Array.from(itensDoMenu).map(function(el) {
+      return el.href;
+    });
+    if (hrefDosItensDeMenu.indexOf(location.hash)) {
+  
+      exibePagina(location.hash);
+    }
+  }
